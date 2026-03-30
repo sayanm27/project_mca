8eb0203ec571730e7b772b30ce238f6fafc3f876
sayanm274 <sayan.m@simplify3x.com>
2026-03-30 17:12:20 +0530

    redoing frontend

--- Backend/app.py
+++ Backend/app.py
@@ -2,9 +2,11 @@ from flask import Flask, jsonify, request
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 import sqlite3
 import utilities
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 app.config["JWT_SECRET_KEY"] = "45969730"  
 jwt = JWTManager(app)
 
